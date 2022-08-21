8bfe709ab39e95d2b7e09bfe5f3182ada9c569b5
20547430 <u20547430@tuks.co.za>
2022-08-21 15:27:57 +0200

    Add project files.

---
+++ u20547430_HW4/Models/Resource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20547430_HW4.Models
+{
+    public abstract class Resource
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public string Subject { get; set; }
+
+        //public Image Image { get; set; }
+    }
+}
