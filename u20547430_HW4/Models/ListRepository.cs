f75428fdfe2f54060624e3a1f0823638f886edd9
20547430 <u20547430@tuks.co.za>
2022-08-21 17:20:27 +0200

    List v1

---
+++ u20547430_HW4/Models/ListRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20547430_HW4.Models
+{
+    public class ListRepository
+    {
+        //past paper
+        
+
+        public static readonly List<PastPaper> pastPapers = new List<PastPaper>
+        {
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+            new PastPaper{Year=2021, Author= "Department of Basic Education", PaperNo=1, Province="South Africa", Subject="Consumer Studies", Title="NSC Consumer Studies", ExamType="November"},
+        };
+
+        //study guide
+
+        public static readonly List<StudyGuide> studyGuides = new List<StudyGuide>
+        {
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+            new StudyGuide{Subject="Accounting", Author="Department of Basic Education", Edition=2, ISBN="978-1-4315-1950--7", Title="(CAPS) Mind the Gap Grade 12 Study Guide Accounting"},
+
+        };
+
+        //textbook
+
+        public static readonly List<Textbook> textbooks = new List<Textbook>
+        {
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"},
+            new Textbook{Subject="Mathematics(maths)", Author="Various Volunteers", Edition=1, ISBN="-", Title="Everything Grade 12 Maths"}
+
+        };
+
+    }
+}
--- u20547430_HW4/Models/PastPaper.cs
+++ u20547430_HW4/Models/PastPaper.cs
@@ -10,22 +10,24 @@ namespace u20547430_HW4.Models
         public int Year { get; set; }
         public string Province { get; set; }
         public int PaperNo { get; set; }
+        public string ExamType { get; set; }
 
         public PastPaper() { }
 
-        public PastPaper(int _year, string _province, int _paperNo ,string _title, string _author, string _subject)
+        public PastPaper(int _year, string _province, int _paperNo ,string _title, string _author, string _subject, string _examType)
             : base(_author, _title, _subject)
         {
            
             Year = _year;
             Province = _province;
             PaperNo = _paperNo;
+            ExamType = _examType;
         }
     
 
        public override sealed string GetInfo()
         {
-            return $"Author: {Author}</br>Subject: {Subject}</br>Year: {Year}</br>Province: {Province}</br>Paper Number: {PaperNo}</br>P";
+            return $"Author: {Author}</br>Subject: {Subject}</br>Year: {Year}</br>Province: {Province}</br>Paper Number: {PaperNo}</br>Exam Type: {ExamType}P";
         }
 
         public override sealed string ToString()
